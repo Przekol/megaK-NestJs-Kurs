1f9d9033c07e009b58368aca43d0b3b93ffbe467
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-07 06:00:35 +0100

    Add basket module, controller, service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopModule } from './shop/shop.module';
+import { BasketModule } from './basket/basket.module';
 
 @Module({
-  imports: [ShopModule],
+  imports: [ShopModule, BasketModule],
   controllers: [AppController],
   providers: [AppService],
 })
