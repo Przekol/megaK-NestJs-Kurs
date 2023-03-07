b7ee58bceacb8539da702446e63fd1dc2a8c518f
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-07 06:14:57 +0100

    Add types, AddProductDto, addProductToBasket and add methods

--- src/basket/basket.service.ts
+++ src/basket/basket.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { AddProductDto } from './dto/add-product.dto';
+import { AddProductToBasketResponse } from '../types';
 
 @Injectable()
-export class BasketService {}
+export class BasketService {
+  private items: Product[] = [];
+
+  add(item: AddProductDto): AddProductToBasketResponse {
+    if (
+      typeof item.name !== 'string' ||
+      typeof item.count !== 'number' ||
+      item.name === '' ||
+      item.count < 1
+    ) {
+      return {
+        isSuccess: false,
+      };
+    }
+
+    this.items.push(item);
+
+    return {
+      isSuccess: true,
+      index: this.items.length - 1,
+    };
+  }
+}
